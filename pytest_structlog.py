85c4dc724aac3893cae01eb22d505b52ecfb235d
Logi <logi.ragnarsson@controlant.com>
2023-04-27 15:02:16 +0200

    Clear contextvars before yielding log, even if the merge_contextvars processor is not present since we don't want `get_contextvars` to leak either.

--- pytest_structlog.py
+++ pytest_structlog.py
@@ -1,13 +1,16 @@
 import os
+
 import pytest
 import structlog
 
 try:
     from structlog.contextvars import merge_contextvars
+    from structlog.contextvars import clear_contextvars
 except ImportError:
     # structolg < 20.1.0
     # use a "missing" sentinel to avoid a NameError later on
     merge_contextvars = object()
+    clear_contextvars = lambda *a, **kw: None
 
 
 __version__ = "0.6"
@@ -91,6 +94,7 @@ def log(monkeypatch, request):
             # see https://github.com/wimglenn/pytest-structlog/issues/20
             new_processors.append(processor)
     new_processors.append(cap.process)
+    clear_contextvars()
     structlog.configure(processors=new_processors, cache_logger_on_first_use=False)
     cap.original_configure = configure = structlog.configure
     cap.configure_once = structlog.configure_once
