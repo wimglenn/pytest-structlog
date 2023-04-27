fba700ae305c1b13560c1705ccdff2b8fddf3feb
Logi <logi.ragnarsson@controlant.com>
2023-04-27 15:01:19 +0200

    Failing test for #24

---
+++ tests/test_issue24.py
@@ -0,0 +1,34 @@
+import os
+
+import pytest
+import structlog
+
+logger = structlog.get_logger()
+
+
+@pytest.fixture(scope="module")
+def issue24_setup():
+    """Isolate this module's contextvars from other tests, even while issue 20 is present"""
+    pytest.importorskip("structlog.contextvars")
+    structlog.contextvars.clear_contextvars()
+    yield
+    structlog.contextvars.clear_contextvars()
+
+
+# Make sure that at least one process runs the test at least twice
+try:
+    RUN_COUNT = int(os.environ.get("PYTEST_XDIST_WORKER_COUNT", 1)) + 1
+except:
+    RUN_COUNT = 2
+
+
+@pytest.mark.parametrize("n", list(range(RUN_COUNT)))
+def test_contextvar_isolation_in_events(issue24_setup, log, n):
+    logger.info("without_context")
+    structlog.contextvars.bind_contextvars(ctx=n)
+    logger.info("with_context")
+    assert log.events == [
+        {"event": "without_context", "level": "info"},
+        {"event": "with_context", "level": "info", "ctx": n},
+    ]
+    assert structlog.contextvars.get_contextvars() == {"ctx": n}
